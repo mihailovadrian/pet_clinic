3139e72668b8dadce99fad7b2d303f1e2942f803
mihailovadrian <adrianmihailov123@gmail.com>
2019-08-22 11:33:31 -0700

    Create SpecialtyJPAService.java

---
+++ pet_clinic_data/src/main/java/com/mihailov/pet_clinic/services/DataJPA/SpecialtyJPAService.java
@@ -0,0 +1,47 @@
+package com.mihailov.pet_clinic.services.DataJPA;
+
+import com.mihailov.pet_clinic.model.Specialty;
+import com.mihailov.pet_clinic.repositories.SpecialtyRepository;
+import com.mihailov.pet_clinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("datajpa")
+public class SpecialtyJPAService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJPAService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
