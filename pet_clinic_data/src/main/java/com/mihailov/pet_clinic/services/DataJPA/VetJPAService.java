579c8e7ece9757cb0838394185b2967743febf18
mihailovadrian <adrianmihailov123@gmail.com>
2019-08-22 11:21:39 -0700

    Added VetJPAService

---
+++ pet_clinic_data/src/main/java/com/mihailov/pet_clinic/services/DataJPA/VetJPAService.java
@@ -0,0 +1,46 @@
+package com.mihailov.pet_clinic.services.DataJPA;
+
+import com.mihailov.pet_clinic.model.Vet;
+import com.mihailov.pet_clinic.repositories.VetRepository;
+import com.mihailov.pet_clinic.services.VetService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VetJPAService implements VetService {
+    private VetRepository vetRepository;
+
+    public VetJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return null;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
